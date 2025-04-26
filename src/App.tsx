b9f0f7a8041b978abf8592ff3338c378dc2e44f6
Eliseu Santos <eliseu.fesantos@gmail.com>
2025-04-26 15:45:25 -0300

    feat: cria pastas e arquivos dos componentes e configura App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { Header } from './components/Header/Header';
+import { HeroSection } from './components/HeroSection/HeroSection';
+import { AboutSection } from './components/AboutSection/AboutSection';
+import { ServicesSection } from './components/ServicesSection/ServicesSection';
+import { ContactSection } from './components/ContactSection/ContactSection';
+
 function App() {
-  return <h1>FIAP Project</h1>;
+  return (
+    <>
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <ServicesSection />
+      <ContactSection />
+    </>
+  );
 }
 
 export default App;
+
